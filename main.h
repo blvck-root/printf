1b678574aac2d83dcb7da71396b9827108223c94
blvck-root <mtndlovu81@gmail.com>
2023-10-16 15:14:40 +0200

    define format struct

--- main.h
+++ main.h
@@ -3,6 +3,20 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
+/**
+ * struct cformat - basic structure of a format processor
+ *
+ * @type: char that signifies the type of arg to be printed
+ * @f: pointer to function that prints arg
+ * Description: structure of an object that converts a format
+ * specifier to the corresponding argument
+ */
+typedef struct cformat
+{
+	char *type;
+	int (*f)(va_list, char *, int);
+} cf_t;
+
 int _printf(const char *format, ...);
 
 #endif
